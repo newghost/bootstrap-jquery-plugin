401391c7789743312ecf713324797d2fd3cf3f42
Krzhang <kris.newghost@gmail.com>
2013-10-18 09:40:20 +0800

    init

---
+++ js/string.format.js
@@ -0,0 +1,16 @@
+/* Extend string method */
+
+/*
+string.format, ref: http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format/4673436#4673436
+*/
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
