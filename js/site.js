c887fb54f52b394d1f5ed2e95bdb390c1e8bb98d
Kris Zhang <c2u@live.cn>
2015-12-12 15:27:36 +0800

    Update API

--- js/site.js
+++ js/site.js
@@ -93,7 +93,7 @@ var apply = function(selector) {
             , {
                   id:         "users"
                 , text:       "Users"
-                , state:      "close"
+                , close:      true
                 , children:   [
                       {text: "Kris"}
                     , {text: "Tom"}
