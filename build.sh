3702da6edec06e9f633d9e0ae58f3dc2a016410f
Kris Zhang <c2u@live.cn>
2015-12-12 14:57:23 +0800

    Rewrite Tree plugin

--- build.sh
+++ build.sh
@@ -3,12 +3,6 @@
 #show commands
 set -x
 
-#combine source files into one
-cd src
-cat copyright.js string.format.js jquery.dialog.js jquery.messager.js jquery.datagrid.js jquery.tree.js > ../js/jquery.bootstrap.js
-cd ..
-
-
 #require uglify js: [sudo]npm install uglify-js -g
 node src/minifier js/jquery.bootstrap.js js/jquery.bootstrap.min.tmp.js
 cat src/copyright.js js/jquery.bootstrap.min.tmp.js > js/jquery.bootstrap.min.js
--- js/jquery.bootstrap.js
+++ js/jquery.bootstrap.js
@@ -250,10 +250,10 @@ $.messager = (function() {
     + '</div>'
     ;
 
-  var $msgbox;
+  var $msgbox = $('.dialog.msg-popup')
 
   var popup = function(message) {
-    if (!$msgbox) {
+    if (!$msgbox.size()) {
       $msgbox = $(msghtml);
       $('body').append($msgbox);
     }
@@ -263,7 +263,7 @@ $.messager = (function() {
 
     setTimeout(function() {
       $msgbox.modal('hide');
-    }, 1500);
+    }, 2000);
   };
 
   return {
@@ -551,88 +551,100 @@ Dependence: string.js
 
   $.fn.tree = function(method, options) {
 
-    var self        = this
-      , $this       = $(self)
-      , pushFn      = Array.prototype.push
-      , icon_node   = "glyphicon-file"
-      , icon_open   = "glyphicon-folder-open"
-      , icon_close  = "glyphicon-folder-close"
-      ;
+    var self          = this
+      , $this         = $(self)
+      , pushFn        = Array.prototype.push
+      , treeClass     = 'nav'
+      , activeClass   = 'active'
+      , folderIcon    = 'glyphicon glyphicon-folder-close'
+      , itemIcon      = 'glyphicon glyphicon-file'
+      , indentIcon    = ''
 
-    var build = function(data, root, parentState) {
+
+    var build = function(data, indent) {
       var tree = [];
-      !root && tree.push('<ul style="display:{0}">'.format(parentState == "close" ? "none" : "block"));
+
+      tree.push('<ul class="{0} {1}">'.format(indent ? '' : 'tree-nav' , treeClass))
 
       for (var i = 0, l = data.length; i < l; i++) {
         var node        = data[i]
           , children    = node.children
           , id          = node.id
-          , state       = node.state
+          , active      = node.active
+          , classed     = node.classed
           , attributes  = node.attributes
-          ;
+          , iconClass   = node.icon || (children ? folderIcon : itemIcon)
 
-        tree.push('<li>');
-        if (1) {
-          var type
-            = typeof children == "undefined"
-            ? icon_node
-            : ( state == "close" ?  icon_close : icon_open );
-          tree.push('<span class="glyphicon {0}"></span> '.format(type));
-        }
-        tree.push('<a{1}{2}{3}>{0}</a>'.format(
-            node.text
-          , children   ? " class='tree-node'" : ""
+
+        tree.push('<li class="{0}">'.format(active ? activeClass : ''))
+
+        icon = '<i class="{0}"></i>'.format(children ? ('tree-folder ' + iconClass) : ('tree-item ' + iconClass))
+
+        var item = '<a{1}{2} class="{3}">{0}</a>'.format(
+            indent + icon + node.text
           , id         ? " data-id='{0}'".format(id) : ""
           , attributes ? " data-attr='{0}'".format(JSON.stringify(attributes)) : ""
-        ));
-        children && pushFn.apply(tree, build(children, false, state));
-        tree.push('</li>');
-      };
-      !root && tree.push('</ul>')
+          , classed || ''
+        )
 
-      return tree;
-    };
+        tree.push(item)
+        children && pushFn.apply(tree, build(children, indent + '<i class="tree-indent {0}"></i>'.format(indentIcon)))
+        tree.push('</li>')
+      }
+
+      tree.push('</ul>')
+
+      return tree
+    }
 
     var bind = function() {
       $("span.glyphicon-folder-open, span.glyphicon-folder-close", $this).click(function(e) {
         var $icon     = $(this)
-          , $children = $icon.closest("li").children("ul");
+          , $children = $icon.closest("li").children("ul")
 
         if ($icon.hasClass(icon_close)){
-          $icon.removeClass(icon_close).addClass(icon_open);
-          $children.show();
+          $icon.removeClass(icon_close).addClass(icon_open)
+          $children.show()
         } else {
-          $icon.removeClass(icon_open).addClass(icon_close);
-          $children.hide();
+          $icon.removeClass(icon_open).addClass(icon_close)
+          $children.hide()
         }
-      });
-    };
-
-    if (method && method.constructor == Object) {
-      var data = method.data;
-      if (data && (data.constructor == Array)) {
-        var htmlArr = build(data, true);
-        $this.html(htmlArr.join(''));
-        $this.data("config", method);
-        bind();
-      }
-
-      var clickHandler = method.onClick;
-      if (clickHandler) {
-        $("li>a", $this).click(function() {
-          var $link = $(this);
-              attrs = $link.attr("data-attr");
-
-          clickHandler.call(self, {
-              id          : $link.attr("data-id")
-            , attributes  : attrs ? JSON.parse(attrs) : {}
-            , text        : $link.text()
-          }, $link);
-        });
-      }
+      })
     }
 
-    return self;
+    if (method && method.constructor == Object) {
+      treeClass   = method.treeClass    || treeClass
+      activeClass = method.activeClass  || activeClass
+      folderIcon  = typeof method.folderIcon == 'undefined' ? folderIcon  : method.folderIcon
+      itemIcon    = typeof method.itemIcon == 'undefined'   ? itemIcon    : method.itemIcon
+      indentIcon  = typeof method.indentIcon == 'undefined' ? indentIcon  : method.indentIcon
+
+      var data = method.data
+      if (data && (data.constructor == Array)) {
+        var htmlArr = build(data, '')
+        $this.html(htmlArr.join(''))
+        $this.data("config", method)
+        bind()
+      }
+
+      var clickHandler = method.onClick
+
+      $("li>a", $this).click(function() {
+        var $link = $(this)
+          , attrs = $link.attr("data-attr")
+
+        $link.closest('li')
+          .toggleClass(activeClass)
+
+        clickHandler && clickHandler.call(self, {
+            id          : $link.attr("data-id")
+          , attributes  : attrs ? JSON.parse(attrs) : {}
+          , text        : $link.text()
+        }, $link)
+      })
+    }
+
+    return self
   };
 
 })(jQuery);
